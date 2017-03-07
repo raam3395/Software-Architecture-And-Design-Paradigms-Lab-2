a243c961e1d9cd9a715437f573460324a04574c5
Nyanaraam <raam3395@gmail.com>
2017-03-07 23:54:04 +0800

    Add files via upload

---
+++ DisplayElement.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salab2;
+
+/**
+ *
+ * @author wes140031
+ */
+public interface DisplayElement {
+    public void display();
+    
+}
---
+++ WeatherStation.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salab2;
+
+/**
+ *
+ * @author wes140031
+ */
+public class WeatherStation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+    
+}
